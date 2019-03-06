380b805d87c39d9ed65bd421abd2301b53353796
houxias <1530364191@qq.com>
2019-03-06 18:46:34 +0800

    建设银行UI

---
+++ app/src/main/java/com/page/android/utils/AppUtils.java
@@ -0,0 +1,40 @@
+package com.page.android.utils;
+
+import android.content.Context;
+import android.util.DisplayMetrics;
+import android.view.WindowManager;
+
+/**
+ * Created by 54hk on 2019/3/6.
+ * 全局 使用的一些方法！
+ */
+
+public class AppUtils {
+    /**
+     * 获得屏幕宽度
+     *
+     * @param context
+     * @return
+     */
+    public static int getScreenWidth(Context context) {
+        WindowManager wm = (WindowManager) context
+                .getSystemService(Context.WINDOW_SERVICE);
+        DisplayMetrics outMetrics = new DisplayMetrics();
+        wm.getDefaultDisplay().getMetrics(outMetrics);
+        return outMetrics.widthPixels;
+    }
+
+    /**
+     * 获得屏幕高度
+     *
+     * @param context
+     * @return
+     */
+    public static int getScreenHeight(Context context) {
+        WindowManager wm = (WindowManager) context
+                .getSystemService(Context.WINDOW_SERVICE);
+        DisplayMetrics outMetrics = new DisplayMetrics();
+        wm.getDefaultDisplay().getMetrics(outMetrics);
+        return outMetrics.heightPixels;
+    }
+}
---
+++ app/src/main/java/com/page/android/utils/GlideUtils.java
@@ -0,0 +1,18 @@
+package com.page.android.utils;
+
+import android.content.Context;
+import android.text.TextUtils;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.page.android.R;
+
+/**
+ * Created by 54hk on 2019/3/5.
+ * //加载图片的工具类
+ */
+
+public class GlideUtils {
+
+}
