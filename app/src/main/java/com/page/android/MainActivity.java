b81850f61de7d3792fba70b1536b427f9ac9e2dc
houxias <1530364191@qq.com>
2019-03-06 19:15:51 +0800

    s

--- app/src/main/java/com/page/android/MainActivity.java
+++ app/src/main/java/com/page/android/MainActivity.java
@@ -41,17 +41,61 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mContext = MainActivity.this;
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        right = (CoordinatorLayout) findViewById(R.id.right);
-        left = (NavigationView) findViewById(R.id.nav_view);
+        initDrawer();
+        initView();
+    }
+
+
+    private void initView() {
+        //初始侧滑的宽度
+        ViewGroup.LayoutParams layoutParams = left.getLayoutParams();
+        layoutParams.width = AppUtils.getScreenWidth(this) / 3 * 2;
+        left.setLayoutParams(layoutParams);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        }
+        DrawerLayout drawer =  findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    //初始化侧滑
+    private void initDrawer() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        right = findViewById(R.id.right);
+        left = findViewById(R.id.nav_view);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
         right.setOnTouchListener(new View.OnTouchListener() {
@@ -88,44 +132,5 @@ public class MainActivity extends AppCompatActivity
             public void onDrawerStateChanged(int newState) {
             }
         });
-        init();
-    }
-
-    private void init() {
-        //初始侧滑的宽度
-        ViewGroup.LayoutParams layoutParams = left.getLayoutParams();
-        layoutParams.width =  AppUtils.getScreenWidth(this) / 3 * 2;
-        left.setLayoutParams(layoutParams);
-    }
-
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }
-
-
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        }
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
     }
 }
